23bae4972106b57ff66ae19370740773be0e673e
diandika <mohammaddicky.andika@gmail.com>
2019-10-08 23:25:52 +0700

    [Ref share] implement top share layout

---
+++ share-top.php
@@ -0,0 +1,7 @@
+<div>
+    <a href='javascript:void(0);' onClick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php echo get_the_permalink(); ?>', '_blank', 'width=600,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=0,screeny=0');" class="ac white rd2 mb10 f16 dip" style="background: #3b5998;width: 50px;padding: 10px 0px; margin: 0px 5px 0px auto;"><i class="fa fa-facebook fa-lg pa10"></i></a>
+
+    <a href='javascript:void(0);' onClick="window.open('https://twitter.com/intent/tweet?original_referer=<?php echo get_the_permalink(); ?>&amp;text=<?php the_title(); ?>&amp;tw_p=tweetbutton&amp;url=<?php echo get_the_permalink(); ?>&amp;via=tribunnews', '_blank', 'width=600,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=0,screeny=0');" class="ac white rd2 mb10 f16 dip" style="background: #4099ff;width: 50px;padding: 10px 0px; margin: 10px auto; margin-right:5px;"><i class="fa fa-twitter fa-lg pa10"></i></a>
+
+    <a href='javascript:void(0);' onClick="window.open('https://plus.google.com/share?url=<?php echo get_the_permalink(); ?>', '_blank', 'width=600,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=0,screeny=0');" class="ac white rd2 mb10 f16 dip" style="background: #d34836;width: 50px;padding: 10px 0px; margin: 10px auto;margin-right:5px;"><i class="fa fa-google-plus fa-lg pa10"></i></a>
+</div>
